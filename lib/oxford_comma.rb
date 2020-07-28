fbbfaaa6a5491f4f2e316f66306272bf61ac6d98
NickSpangler <Spangleyman@mac.com>
2020-07-28 16:20:10 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
-end
+   if array.length == 1
+    string_array = array.join
+   elsif array.length == 2
+    string_array = array.join(" and ")
+   elsif array.length >= 3
+    string_array = ""
+    counter = 0
+    while counter <= array.length - 2
+        string_array << "#{array[counter]}, "
+        counter += 1
+    end
+    string_array << "and #{array.last}"
+   end
+  string_array
+end 
